4c5a082cd51583967c5de39995a56a9fba371bf2
maheshraja <i.mahesh2004@gmail.com>
2024-07-22 23:21:38 +0100

    small change prettier

--- src/app.js
+++ src/app.js
@@ -1,26 +1,32 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
-app.use(express.json({
-    limit: "16kb"
-}));
+app.use(
+  express.json({
+    limit: '16kb',
+  })
+);
 
 // Make URL Params as encoded
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true,
-    limit: "16kb"
-}));
+    limit: '16kb',
+  })
+);
 
-app.use(express.static("public")); // folder to add static assets
+app.use(express.static('public')); // folder to add static assets
 
 app.use(cookieParser());
 
-export {app} 
+export { app };
